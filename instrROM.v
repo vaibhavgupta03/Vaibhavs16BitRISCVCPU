29591ac65e4e409f537957cc79df95660a757dcc
Vaibhav Gupta <vaibhavgupta6393@gmail.com>
2026-04-22 10:13:20 +0530

    Rewrite  instrROM.v for improved clarity

--- instrROM.v
+++ instrROM.v
@@ -8,11 +8,10 @@ module instrROM(
     
 
     initial begin
-        // Zero-fill first so unassigned words are known
+        
         for (i = 0; i < 64; i = i + 1)
             mem[i] = 16'h0000;
-        // Load assembled program - file must be in xsim working directory:
-        // D:/RISCpakka/RISCpakka.sim/sim_1/behav/xsim/prog.hex
+        
         $readmemh("prog.hex", mem);
     end
 
